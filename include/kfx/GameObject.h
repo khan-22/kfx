738aa3f5f0cee927d4925fd5ee43d7ecfa124784
Karl H <khansson.nr2@gmail.com>
2017-07-16 17:48:54 +0200

    Created a basic GameObject class

---
+++ include/kfx/GameObject.h
@@ -0,0 +1,28 @@
+/*************************/
+/* (C) Karl Hansson 2017 */
+/*************************/
+#pragma once
+
+#ifndef GAME_OBJECT_H
+#define GAME_OBJECT_H
+
+#include <array>
+
+#include "Components.h"
+#include "Handle.h"
+
+namespace kfx {
+class GameObject {
+ public:
+  GameObject();
+
+  void addComponent(ComponentType type, Handle handle);
+  Handle getComponent(ComponentType type);
+  bool hasComponent(ComponentType type);
+
+ private:
+  std::array<Handle, ComponentType::NUM_TYPES> m_components;
+};
+}
+
+#endif  // GAME_OBJECT_H
---
+++ src/GameObject.cpp
@@ -0,0 +1,29 @@
+#include "kfx/GameObject.h"
+
+#include <cassert>
+
+namespace kfx {
+GameObject::GameObject() {}
+
+void GameObject::addComponent(ComponentType type, Handle handle) {
+  assert(type >= 0);
+  assert(type < ComponentType::NUM_TYPES);
+  assert(handle.m_is_initialized == true);
+
+  m_components[type] = handle;
+}
+
+Handle GameObject::getComponent(ComponentType type) {
+  assert(type >= 0);
+  assert(type < ComponentType::NUM_TYPES);
+
+  return m_components[type];
+}
+
+bool GameObject::hasComponent(ComponentType type) {
+  assert(type >= 0);
+  assert(type < ComponentType::NUM_TYPES);
+
+  return m_components[type].m_is_initialized;
+}
+}
