e5b2cc9f20786b4b81cafa4165bef04308d422f3
Karl H <khansson.nr2@gmail.com>
2017-07-16 17:49:49 +0200

    Created the base class ( / Interface) for Systems.

---
+++ include/kfx/system/System.h
@@ -0,0 +1,24 @@
+/*************************/
+/* (C) Karl Hansson 2017 */
+/*************************/
+#pragma once
+
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include "kfx/GameObject.h"
+#include "kfx/GameObjectFactory.h"
+
+namespace kfx {
+class System {
+ public:
+  virtual ~System();
+
+  virtual void update(float dt) = 0;
+
+ protected:
+  System(GameObjectFactory* game_object_factory);
+  GameObjectFactory* m_game_object_factory;
+};
+}
+#endif  // SYSTEM_H
