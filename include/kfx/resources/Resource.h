656939bfb4e640c06ec43b711b00f0dfa4267a3e
Karl H <khansson.nr2@gmail.com>
2017-07-17 22:49:57 +0200

    Created a wrapper struct (Resource) which is meant to hold the handle as well as the resource itself

---
+++ include/kfx/resources/Resource.h
@@ -0,0 +1,19 @@
+/*************************/
+/* (C) Karl Hansson 2017 */
+/*************************/
+#pragma once
+
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include "kfx/Handle.h"
+
+namespace kfx {
+template <typename T>
+struct Resource {
+ public:
+  Handle handle;
+  T value;
+};
+}
+#endif  // RESOURCE_H
