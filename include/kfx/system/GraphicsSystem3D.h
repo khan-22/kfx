afd70aa1727d140df63fb9c17a77bb35686cc2da
Karl H <khansson.nr2@gmail.com>
2017-07-17 22:55:22 +0200

    Adjusted GraphicsSystem3D to the changes made to GameObjectFactory and MeshManager

--- include/kfx/system/GraphicsSystem3D.h
+++ include/kfx/system/GraphicsSystem3D.h
@@ -6,17 +6,20 @@
 #ifndef GRAPHICS_SYSTEM_3D_H
 #define GRAPHICS_SYSTEM_3D_H
 
+#include "kfx/resources/MeshManager.h"
 #include "kfx/system/System.h"
 
 namespace kfx {
 class GraphicsSystem3D : public System {
  public:
-  GraphicsSystem3D(GameObjectFactory* game_object_factory);
-  virtual ~GraphicsSystem3D();
+  GraphicsSystem3D(GameObjectFactory& game_object_factory,
+                   MeshManager& mesh_manager);
+  ~GraphicsSystem3D() override;
 
   void update(float dt) final override;
 
  private:
+  MeshManager& m_mesh_manager;
 };
 }
 #endif  // 3D_GRAPHICS_SYSTEM_H
--- src/system/GraphicsSystem3D.cpp
+++ src/system/GraphicsSystem3D.cpp
@@ -1,11 +1,13 @@
 #include "kfx/system/GraphicsSystem3D.h"
 
 // TEMPORARY!!
+#include <GL/glew.h>
 #include <iostream>
 
 namespace kfx {
-GraphicsSystem3D::GraphicsSystem3D(GameObjectFactory* game_object_factory)
-    : System(game_object_factory) {
+GraphicsSystem3D::GraphicsSystem3D(GameObjectFactory& game_object_factory,
+                                   MeshManager& mesh_manager)
+    : System(game_object_factory), m_mesh_manager(mesh_manager) {
   // ...
 }
 
@@ -14,21 +16,37 @@ GraphicsSystem3D::~GraphicsSystem3D() {
 }
 
 void GraphicsSystem3D::update(float dt) {
-  // ...
+  HandledResource<GameObject>& game_objects =
+      m_game_object_factory.getGameObjects();
 
-  HandleManager& handle_manager = m_game_object_factory->getHandleManager();
+  // HandleManager& mesh_handle_manager = m_mesh_manager.getHandleManager();
+  HandledResource<Mesh>& meshes = m_mesh_manager.getMeshes();
 
-  for (auto& object : m_game_object_factory->getGameObjects()) {
+  for (auto& object : game_objects.resource) {
     std::cout << "Object has component? ["
-              << (object.hasComponent(ComponentType::TRANSFORM) ? "YES]"
-                                                                : "NO]")
+              << (object.value.hasComponent(ComponentType::MESH) ? "YES]"
+                                                                 : "NO]")
               << std::endl;
 
-    TransformComponent* transform_component = static_cast<TransformComponent*>(
-        handle_manager.getEntry(object.getComponent(ComponentType::TRANSFORM)));
+    TransformComponent* transform_component =
+        m_game_object_factory.getTransformComponents().getResourceEntry(
+            object.value.getComponent(ComponentType::TRANSFORM));
 
     transform_component->x += 0.01f;
     std::cout << "X: " << transform_component->x << std::endl;
+
+    MeshComponent* mesh_component =
+        m_game_object_factory.getMeshComponents().getResourceEntry(
+            object.value.getComponent(ComponentType::MESH));
+
+    // Mesh* mesh = static_cast<Mesh*>(
+    //     mesh_handle_manager.getEntry(mesh_component->mesh_handle));
+    Mesh* mesh = 
+        meshes.getResourceEntry(mesh_component->mesh_handle);
+
+    glBindVertexArray(mesh->vertex_array_object);
+    glDrawArrays(GL_TRIANGLES, 0, mesh->vertices.size());
+    glBindVertexArray(0);
   }
 }
 }
