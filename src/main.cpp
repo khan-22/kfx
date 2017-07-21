95bc77bbd008bdf5dc04899c40db30c031331256
Karl H <khansson.nr2@gmail.com>
2017-07-21 23:47:21 +0200

    Removed old code from main

--- src/main.cpp
+++ src/main.cpp
@@ -50,10 +50,11 @@ class KeyInputListener : public Listener {
   void tell(Message message) final override {
     // std::cout << "(" << glfwGetKeyName(message.key_input_key, 0) << ")" <<
     // std::endl;
-    printf("%s", message.key_input_action == GLFW_PRESS
-                     ? "PRESS: "
-                     : message.key_input_action == GLFW_RELEASE ? "RELEASE: "
-                                                                : "REPEAT: ");
+    printf("%s",
+           message.key_input_action == GLFW_PRESS
+               ? "PRESS: "
+               : message.key_input_action == GLFW_RELEASE ? "RELEASE: "
+                                                          : "REPEAT: ");
 
     if (message.key_input_mods & GLFW_MOD_ALT) {
       printf("%s", "<ALT>");
@@ -78,16 +79,6 @@ class KeyInputListener : public Listener {
 
  private:
 };
-}
-
-static void key_callback(GLFWwindow *window, int key, int scancode, int action,
-                         int mods);
-
-static void key_callback(GLFWwindow *window, int key, int scancode, int action,
-                         int mods) {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
-  }
 }
 
 GLFWwindow *initialize();
@@ -118,78 +109,6 @@ GLFWwindow *initialize() {
   glfwSetKeyCallback(window, key_callback);
 
   return window;
-}
-
-GLuint create_shader();
-GLuint create_shader() {
-  GLuint program = glCreateProgram();
-
-  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-
-  {
-    std::ifstream shaderFile(std::string("./res/shaders/basic.vs"));
-    std::string shaderText((std::istreambuf_iterator<char>(shaderFile)),
-                           std::istreambuf_iterator<char>());
-    shaderFile.close();
-    const char *shaderTextPtr = shaderText.c_str();
-    std::cout << shaderText << std::endl;
-    glShaderSource(vs, 1, &shaderTextPtr, nullptr);
-    glCompileShader(vs);
-
-    GLint success = 0;
-    GLchar vsError[1024] = {0};
-    glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
-    if (success == GL_FALSE) {
-      glGetShaderInfoLog(vs, sizeof(vsError), nullptr, vsError);
-
-      std::cout << ">> VERTEX SHADER ERRORS -------------------------"
-                << std::endl;
-      std::cout << std::endl << vsError << std::endl;
-    }
-
-    glAttachShader(program, vs);
-  }
-
-  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-
-  {
-    std::ifstream shaderFile(std::string("./res/shaders/basic.fs"));
-    std::string shaderText((std::istreambuf_iterator<char>(shaderFile)),
-                           std::istreambuf_iterator<char>());
-    shaderFile.close();
-    const char *shaderTextPtr = shaderText.c_str();
-    std::cout << shaderText << std::endl;
-    glShaderSource(fs, 1, &shaderTextPtr, nullptr);
-    glCompileShader(fs);
-
-    GLint success = 0;
-    GLchar fsError[1024] = {0};
-    glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
-    if (success == GL_FALSE) {
-      glGetShaderInfoLog(fs, sizeof(fsError), nullptr, fsError);
-
-      std::cout << ">> FRAGMENT SHADER ERRORS -------------------------"
-                << std::endl;
-      std::cout << std::endl << fsError << std::endl;
-    }
-    glAttachShader(program, fs);
-  }
-  glLinkProgram(program);
-
-  GLint success = 0;
-  glGetProgramiv(program, GL_LINK_STATUS, &success);
-  if (success == GL_FALSE) {
-    GLchar error[1024] = {0};
-
-    glGetProgramInfoLog(program, sizeof(error), nullptr, error);
-
-    std::cout << ">> LINKING ERRORS -------------------------" << std::endl;
-    std::cout << error << std::endl;
-    // std::cout << std::endl;
-    // std::cout << shaderTextPtr;
-  }
-
-  return program;
 }
 
 int main() {
@@ -278,7 +197,6 @@ int main() {
       1, 5, 6, 1, 6, 2,  // Top
 
       0, 4, 7, 0, 7, 3,  // Bottom
-
   };
 
   mesh_manager.loadMeshFromMemory("test", vertices, indices);
