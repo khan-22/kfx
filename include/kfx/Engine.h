9f5a67b8cbfbf6fd527981520a716d3fcbdbc274
Karl H <khansson.nr2@gmail.com>
2017-07-23 15:51:38 +0200

    Engine now provides references to MeshManager, ShaderManager and GameObjectFactory

---
+++ include/kfx/Engine.h
@@ -0,0 +1,46 @@
+/*************************/
+/* (C) Karl Hansson 2017 */
+/*************************/
+#pragma once
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <memory>
+#include <vector>
+
+#include "kfx/GameObjectFactory.h"
+
+#include "kfx/system/System.h"
+
+#include "kfx/system/GraphicsSystem3D.h"
+
+#include "kfx/resources/MeshManager.h"
+#include "kfx/resources/ShaderManager.h"
+
+namespace kfx {
+class Engine {
+ public:
+  Engine();
+  virtual ~Engine();
+
+  virtual void init();
+
+  virtual void update(float dt);
+  virtual void render();
+
+  GameObjectFactory& getGameObjectFactory();
+
+  MeshManager& getMeshManager();
+  ShaderManager& getShaderManager();
+
+ protected:
+  std::vector<std::unique_ptr<System>> m_systems;
+
+  GameObjectFactory m_game_object_factory;
+
+  MeshManager m_mesh_manager;
+  ShaderManager m_shader_manager;
+};
+}
+#endif  // ENGINE_H
---
+++ src/Engine.cpp
@@ -0,0 +1,39 @@
+#include "kfx/Engine.h"
+
+namespace kfx {
+Engine::Engine() : m_game_object_factory(m_mesh_manager, m_shader_manager) {}
+
+Engine::~Engine() {
+  // ...
+}
+
+void Engine::init() {
+  // ...
+
+  m_systems.push_back(std::make_unique<GraphicsSystem3D>(this));
+}
+
+void Engine::update(float dt) {
+  // ...
+
+  for (auto& system : m_systems) {
+    system->update(dt);
+  }
+}
+
+void Engine::render() {
+  // ...
+}
+
+GameObjectFactory& Engine::getGameObjectFactory() {
+  return m_game_object_factory;  // <-
+}
+
+MeshManager& Engine::getMeshManager() {
+  return m_mesh_manager;  // <-
+}
+
+ShaderManager& Engine::getShaderManager() {
+  return m_shader_manager;  // <-
+}
+}
