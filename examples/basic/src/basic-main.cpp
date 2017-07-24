1c7dac081eca17cd98afe7e90789d5d73713662c
Karl H <khansson.nr2@gmail.com>
2017-07-24 21:21:39 +0200

    Added the foundation of examples to the project

---
+++ examples/basic/src/basic-main.cpp
@@ -0,0 +1,15 @@
+/***********************************/
+/* Example: basic   */
+/* By: Karl Hansson 2017           */
+/***********************************/
+
+#include <iostream>
+
+int main() {
+  std::cout << "Example: basic" << std::endl << std::endl;
+
+  std::cin.ignore();
+  std::cin.get();
+
+  return 0;
+}
