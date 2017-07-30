7fea37f6d1ca7f6b7436af4e0f269c58eefc13fb
Karl H <khansson.nr2@gmail.com>
2017-07-30 16:55:59 +0200

    basic example now creates a window

--- examples/basic/src/basic-main.cpp
+++ examples/basic/src/basic-main.cpp
@@ -5,6 +5,8 @@
 
 #include <iostream>
 
+#include <kfx/Window.h>
+
 int main() {
   std::cout << "Example: basic" << std::endl << std::endl;
 
@@ -13,6 +15,12 @@ int main() {
 #else
   std::cout << "Release" << std::endl;
 #endif  // NDEBUG
+
+  kfx::Window window(640, 480, "basic example");
+
+  window.setClearColor(0.f, 0.f, 0.f);
+  window.clear();
+  window.swapBuffers();
 
   std::cin.ignore();
   std::cin.get();
