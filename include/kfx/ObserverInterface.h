a0ed03d74cb04610ea9a616c1f3d25421c5bf6e4
Karl H <khansson.nr2@gmail.com>
2017-07-31 00:26:27 +0200

    Added basic observer interface

---
+++ include/kfx/ObserverInterface.h
@@ -0,0 +1,25 @@
+/*************************/
+/* (C) Karl Hansson 2017 */
+/*************************/
+#pragma once
+
+#ifndef OBSERVER_INTERFACE_H
+#define OBSERVER_INTERFACE_H
+
+#include <vector>
+
+namespace kfx {
+
+struct ObserverArgument {
+  uint32_t type;
+  void* data;
+};
+
+class ObserverInterface {
+ public:
+  virtual void notify(ObserverArgument arg) = 0;
+
+ private:
+};
+}
+#endif  // OBSERVER_INTERFACE_H
