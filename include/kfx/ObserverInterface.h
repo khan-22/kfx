9c916133e20914780aa674a220ecdab7567a91d4
Karl H <khansson.nr2@gmail.com>
2017-07-31 13:18:40 +0200

    changed observer function from notify to onNotify

--- include/kfx/ObserverInterface.h
+++ include/kfx/ObserverInterface.h
@@ -13,16 +13,11 @@ namespace kfx {
 struct ObserverArgument {
   uint32_t type;
   void* data;
-<<<<<<< HEAD
-=======
-
-  bool containsData() { return data != nullptr; }
->>>>>>> 5e208c023b0609bf2cc0125c49693fa640a5de73
 };
 
 class ObserverInterface {
  public:
-  virtual void notify(ObserverArgument arg) = 0;
+  virtual void onNotify(ObserverArgument arg) = 0;
 
  private:
 };
