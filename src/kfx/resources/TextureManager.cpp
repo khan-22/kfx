5a6c6d6a306c357e81709f31e114efe6be6429ad
Karl H <khansson.nr2@gmail.com>
2017-08-08 00:19:18 +0200

    Added TextureManager

---
+++ src/kfx/resources/TextureManager.cpp
@@ -0,0 +1,38 @@
+#include "kfx/resources/TextureManager.h"
+
+#include "kfx/resources/FileUtil.h"
+#include "kfx/resources/TextureUtil.h"
+
+namespace kfx {
+Handle TextureManager::loadTextureFromFile(const std::string path) {
+  std::string texture_name = util::breakDownPath(path).back();
+
+  // If the texture has already been loaded, return the handle
+  Handle found_handle = getTextureByName(texture_name);
+  if (found_handle != Handle::NULL_HANDLE) {
+    return found_handle;
+  }
+
+  Handle texture_handle = m_textures.addResourceEntry();
+  if (texture_handle == Handle::NULL_HANDLE) {
+    return Handle::NULL_HANDLE;
+  }
+  Image image = util::loadImage(path);
+
+  Texture* texture = m_textures.getResourceEntry(texture_handle);
+  *texture = util::textureFromImage(image);
+
+  m_name_to_handle_map[texture_name] = texture_handle;
+
+  return texture_handle;
+}
+
+Handle TextureManager::getTextureByName(const std::string name) {
+  auto it = m_name_to_handle_map.find(name);
+  if (it != m_name_to_handle_map.end()) {
+    return it->second;
+  } else {
+    return Handle::NULL_HANDLE;
+  }
+}
+}
