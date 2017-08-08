eecace2c0368ea6e684b9e9ed2203318288c71a5
Karl H <khansson.nr2@gmail.com>
2017-08-08 16:42:21 +0200

    Added return statement to textureFromImage function

--- src/kfx/resources/TextureUtil.cpp
+++ src/kfx/resources/TextureUtil.cpp
@@ -35,10 +35,18 @@ Texture textureFromImage(const Image& image) {
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+  //                 GL_LINEAR_MIPMAP_LINEAR);
+  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   // glGenerateMipmap(GL_TEXTURE_2D);
 
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.width, texture.height, 0,
                GL_RGBA, GL_UNSIGNED_BYTE, image.data.data());
+
+  // Unbind
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  return texture;
 }
 }
 }
